c615747688a79e91e3f2d0acfdee19023f0b29b9
friederjanmoerman <hijbrid@gmail.com>
2018-10-30 15:49:54 +0100

    final changes like SEO optimalization and adding the contact button

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,21 @@
 document.addEventListener('DOMContentLoaded',function(event){
+
   /*
-   * typewriter animation text script
+   * typewriter animation text
    */
 
-  // array with texts to type in typewriter
   var sentenceHardcodepunk = ["Here to fuck some code up"];
-  var sentenceTerminal = ["Design and destroy"];
-  // type one text in the typwriter
-  // keeps calling itself until the text is finished
   function typeWriter(text, i, fnCallback) {
-    // chekc if text isn't finished yet
     if (i < (text.length)) {
-      // add next character to h1
       document.getElementById("sentence__hardcodepunk").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true" class="textbox__underscore">&#95;</span>';
-      // wait for a while and call this function again for next character
       setTimeout(function() {
         typeWriter(text, i + 1, fnCallback)
       }, 100);
     }
-    // text finished, call callback if there is a callback function
     else if (typeof fnCallback == 'function') {
-      // call callback after timeout
       setTimeout(fnCallback, 700);
     }
   }
-  // start a typewriter animation for a text in the sentenceHardcodepunk array
   function StartTextAnimation(i) {
     if (typeof sentenceHardcodepunk[i] == 'undefined'){
       setTimeout(function() {
@@ -32,31 +23,26 @@ document.addEventListener('DOMContentLoaded',function(event){
       }, 20000);
     }
 
-    // check if dataText[i] exists
     if (i < sentenceHardcodepunk[i].length) {
-      // text exists! start typewriter animation
       typeWriter(sentenceHardcodepunk[i], 0, function(){
-        // after callback (and whole text has been animated), start next text
         StartTextAnimation(i + 1);
       });
     }
   }
-  // start the text animation
+
   StartTextAnimation(0);
 
   /*
-   * toggle script
+   * toggle
    */
 
   var toggle = function (elem) {
   	elem.classList.toggle('is-displayed');
   };
 
-  // Listen for click events
   document.addEventListener('click', function (event) {
     var clickedElement = event.target;
 
-  	// Make sure clicked element is our toggle
   	if (!clickedElement.classList.contains('js-toggle')) return;
 
     clickedElement.classList.toggle('is-active');
@@ -67,15 +53,28 @@ document.addEventListener('DOMContentLoaded',function(event){
       clickedElement.innerHTML = "View synopsis";
     }
 
-  	// Prevent default link behavior
   	event.preventDefault();
 
-  	// Get the content
   	var content = document.querySelector(clickedElement.hash);
   	if (!content) return;
 
-  	// Toggle the content
   	toggle(content);
 
   }, false);
+
+  /*
+   * scroll to bottom
+   */
+
+  (function() {
+    'use strict';
+
+    var btnScrollDown = document.querySelector('#scroll-to-contact');
+
+    function scrollDown() {
+      window.scrollTo(0,document.body.scrollHeight);
+    }
+
+    btnScrollDown.addEventListener('click', scrollDown);
+  })();
 });
